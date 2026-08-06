039fbed7e84398ffafae8c6aedd4f5c59c805335
Bahrami <m.m.bhrm85@gmail.com>
2026-08-06 15:35:26 +0330

    Add implicit operator to `Id<T>`.

--- api/src/Domain/Common/Id.cs
+++ api/src/Domain/Common/Id.cs
@@ -3,4 +3,6 @@ namespace App.Domain.Common;
 public readonly record struct Id<T>(Guid Value) {
     public static Id<T> New() => new(Guid.NewGuid());
     public static Id<T> FromGuid(Guid value) => new(value);
+
+    public static implicit operator Guid(Id<T> id) => id.Value;
 }
