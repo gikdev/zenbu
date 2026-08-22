4e4b6fb563dd3a137397dfff1f45b526e1ebecd6
Bahrami <m.m.bhrm85@gmail.com>
2026-08-22 17:30:01 +0330

    Add endpoints & migrations too.

--- api/src/Application/Features/Jot/Jottings/Create/CreateJottingCommandHandler.cs
+++ api/src/Application/Features/Jot/Jottings/Create/CreateJottingCommandHandler.cs
@@ -1,6 +1,7 @@
 using App.Application.Abstractions.Data;
 using App.Application.Abstractions.Identity;
 using App.Application.Abstractions.Messaging;
+using App.Domain.Common;
 using App.Domain.Models.Jot;
 
 namespace App.Application.Features.Jot.Jottings.Create;
@@ -8,8 +9,8 @@ namespace App.Application.Features.Jot.Jottings.Create;
 public sealed class CreateJottingCommandHandler(
     IAppDbContext db,
     ICurrentUser user
-) : ICommandHandler<CreateJottingCommand, JottingResponse> {
-    public async Task<JottingResponse> HandleAsync(CreateJottingCommand command, CancellationToken cancellationToken = default) {
+) : ICommandHandler<CreateJottingCommand, Result<JottingResponse>> {
+    public async Task<Result<JottingResponse>> HandleAsync(CreateJottingCommand command, CancellationToken cancellationToken = default) {
         var newJotting = new Jotting {
             Content = command.Content,
             OwnerId = user.UserIdGuid,
@@ -19,6 +20,6 @@ public sealed class CreateJottingCommandHandler(
         db.Jottings.Add(newJotting);
         await db.SaveChangesAsync(cancellationToken);
 
-        return JottingResponse.FromDomain(newJotting);
+        return Result.Success(JottingResponse.FromDomain(newJotting));
     }
 }
